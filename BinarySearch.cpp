689f04ac75e6312f3fea07904d2d827f89f1549f
Archie Nshindano <archienshindano@gmail.com>
2025-01-08 09:06:06 +0200

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,16 +2,19 @@
 #include <iostream>
 using namespace std;
 
+//CHANGES MADE = NAMING, SPACING , YOUR CODE WAS STUCK IN A LOOP IF I SEARCH FOR A NEGATIVE NUMBER ,
+
 int binarySearch
 (int array[], int searchedNumber, int lowerIndex, int upperIndex) {
     
-    while (lowerIndex < upperIndex) {
+    while (lowerIndex < upperIndex) { // changed <= to <
         
-        int middleIndex =  (upperIndex + lowerIndex) / 2;
+        int middleIndex =  (upperIndex + lowerIndex) / 2; //  low + (high-low)/2  is irrelevant and overcomplicating everything
         
         if (array[mid] == searchedNumber)
             return middleIndex;
-            
+
+        // Adding or subtracting one is irrelevant 
         if (array[mid] < searchedNumber)
             lowerIndex = middleIndex ;
             
@@ -24,9 +27,14 @@ int binarySearch
 }
 
 int main() {
+
+    // space your code , your code was ugly 
     
     int array[] = {2, 3, 4, 10, 40};
     int searchedNumber = -11;
+
+
+    // use proper naming for others to understand
     
     int sizeOfArray = sizeof(array) / sizeof(array[0]);
     int result = binarySearch(array, x, 0, sizeOfArray - 1);
